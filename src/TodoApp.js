6e2c5d90c4e7c8ddde01f562e040f4ce01a35fc6
Anmol Pandey <anmol@classpro.in>
2018-10-25 15:04:31 +0530

    First Commit

---
+++ src/TodoApp.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
+import AddTodo from "./containers/AddTodo";
+import VisibleTodos from "./containers/VisibleTodos";
+
+class TodoApp extends Component {
+  state = {
+    todos: [],
+    visibilityFilter: "SHOW_ALL"
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <AddTodo />
+        <View>
+          <VisibleTodos />
+        </View>
+      </View>
+    );
+  }
+}
+export default TodoApp;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40
+  }
+});
---
+++ src/containers/AddTodo.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { View, TextInput, TouchableOpacity, Image } from "react-native";
+import { connect } from "react-redux";
+
+class AddTodo extends Component {
+  state = {
+    text: ""
+  };
+
+  addTodo = text => {
+    //update redux store
+    this.props.dispatch({ type: "ADD_TODO", text });
+    this.setState({
+      text: ""
+    });
+  };
+  render() {
+    return (
+      <View style={{ flexDirection: "row", marginHorizontal: 20 }}>
+        <TextInput
+          value={this.state.text}
+          onChangeText={text => this.setState({ text })}
+          placeholder="Eg. Create a new Video"
+          style={{
+            flexDirection: "row",
+            borderWidth: 1,
+            borderColor: "#f2f2e1",
+            backgroundColor: "#eaeaea",
+            height: 50,
+            flex: 1,
+            padding: 5
+          }}
+        />
+        <TouchableOpacity onPress={() => this.addTodo(this.state.text)}>
+          <View
+            style={{
+              height: 50,
+              backgroundColor: "#eaeaea",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <Image
+              style={{ width: 30, height: 30, padding: 5 }}
+              source={require("../../assets/Plus_symbol.png")}
+            />
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+export default connect()(AddTodo);
